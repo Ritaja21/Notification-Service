7cf3a57b4ae7c9c1dfda12deb1789d2727966998
Ritaja Tarafder <115881974+Ritaja21@users.noreply.github.com>
2025-05-18 20:47:59 +0530

    Update redis.js

--- config/redis.js
+++ config/redis.js
@@ -4,7 +4,7 @@ const connection = new IORedis({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
     password: process.env.REDIS_PASSWORD,
-    maxRetriesPerRequest: null,       // ✅ this line fixes the error
+    maxRetriesPerRequest: null,      
     enableReadyCheck: false
 });
 
